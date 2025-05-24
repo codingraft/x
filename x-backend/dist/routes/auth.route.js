1414f7d665de6ab8d0c91c7de63a01676c816f5b
Vaibhav <gvaibhav864@gmail.com>
2025-05-24 20:27:04 +0530

    Delete x-backend/dist directory

--- x-backend/dist/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getMe, login, logout, signUp } from '../controllers/auth.controller.js';
-import { protectedRoute } from '../middlewares/protectedRoute.js';
-const router = express.Router();
-router.get('/me', protectedRoute, getMe);
-router.post('/signup', signUp);
-router.post('/login', login);
-router.post('/logout', logout);
-export default router;
--- x-backend/dist/routes/notifications.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { protectedRoute } from '../middlewares/protectedRoute.js';
-import { deleteNotifications, getAllNotifications } from '../controllers/notifications.controller.js';
-const router = express.Router();
-router.get('/', protectedRoute, getAllNotifications);
-router.delete('/', protectedRoute, deleteNotifications);
-export default router;
